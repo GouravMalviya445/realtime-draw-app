160b9cd6915e973ec77a13c615f8b6e3cbcc470d
GouravMalviya445 <gouravmalviya445@gmail.com>
2025-03-01 09:43:04 +0530

    add tscofig file

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,1 +1,17 @@
 import express from "express"
+
+const app = express();
+
+const baseApiRoute = "/api/v1";
+
+app.post(`${baseApiRoute}/signup`, (req, res) => {
+
+});
+
+app.post(`${baseApiRoute}/signin`, (req, res) => {
+
+});
+
+app.post(`${baseApiRoute}/add`, (req, res) => {
+
+});
