7bf5007e5d39d5db288ee1c57a1f9cbca511754b
GouravMalviya445 <gouravmalviya445@gmail.com>
2025-03-10 19:48:57 +0530

    add current-user route

--- apps/http-server/src/routes/user.routes.ts
+++ apps/http-server/src/routes/user.routes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { userSignin, userSignup } from "../controllers/user.controller";
+import { getCurrentUser, userSignin, userSignup } from "../controllers/user.controller";
+import { authMiddleware } from "../middlewares/auth.middleware";
 
 const router: Router = Router();
 
 router.route("/signup").post(userSignup);
 router.route("/signin").post(userSignin);
+
+// secure routes
+router.route("/current-user").get(authMiddleware, getCurrentUser)
 
 export default router;
