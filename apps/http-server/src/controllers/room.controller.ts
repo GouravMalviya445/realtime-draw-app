9cca2e3942f8d74994c45442c0db92ce076b013d
GouravMalviya445 <gouravmalviya445@gmail.com>
2025-03-11 13:06:01 +0530

    update room controller

--- apps/http-server/src/controllers/room.controller.ts
+++ apps/http-server/src/controllers/room.controller.ts
@@ -1,10 +1,45 @@
 import { requestHandler, ApiError, ApiResponse } from "@repo/be-common/src/utils";
+import { createRoomValidation } from "@repo/common/zod"
+import { prisma } from "@repo/db/prisma";
 
 const createRoom = requestHandler(async (req, res) => {
+  const { success, data, error } = createRoomValidation.safeParse(req.body);
+  if (!success) {
+    throw new ApiError(400, "Invalid inputs", error.errors, error.stack);
+  }
+
   try {
+    // slugify the name and store the slug
+    const slug = data.name.split(" ").join("-").toLowerCase().trim();
+    
     // db calls  
+    // check room exist
+    const existingRoom = await prisma.room.findUnique({
+      where: { slug }
+    })
+    if (existingRoom) {
+      throw new ApiError(400, "Room with this name is already exist");
+    }
+
+    // create new room
+    const newRoom = await prisma.room.create({
+      data: {
+        slug,
+        adminId: req?.user?.id as string
+      }
+    })
+
+    return res
+      .status(201)
+      .json(new ApiResponse(
+        201,
+        "Room is successfully created",
+        { room: newRoom }
+      ))
+
+
   } catch (error: any) {
-    throw new ApiError(500, "Error while creating room", [], error.stack)
+    throw new ApiError(500, error.message || "Error while creating room", [], error.stack)
   }
 })
 
