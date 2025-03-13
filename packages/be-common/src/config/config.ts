31d1b370981514cac15d46da4c78c15155fed1e8
GouravMalviya445 <gouravmalviya445@gmail.com>
2025-03-13 14:50:06 +0530

    add corsOrigin

--- packages/be-common/src/config/config.ts
+++ packages/be-common/src/config/config.ts
@@ -8,7 +8,8 @@ const _config = {
   accessTokenSecret: process.env.ACCESS_TOKEN_SECRET,
   accessTokenExpiry: process.env.ACCESS_TOKEN_EXPIRY,
 
-  nodeEnv: process.env.NODE_ENV
+  nodeEnv: process.env.NODE_ENV,
+  corsOrigin: process.env.CORS_ORIGIN
 }
 
 export const config = Object.freeze(_config);
