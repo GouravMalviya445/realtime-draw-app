24de0271e9c216faeea311b79abf5eb28b62ac04
GouravMalviya445 <gouravmalviya445@gmail.com>
2025-03-14 22:15:50 +0530

    add login route

--- apps/web/src/app/(user)/login/page.tsx
+++ apps/web/src/app/(user)/login/page.tsx
@@ -16,28 +16,21 @@ export default function Signup() {
     defaultValues: {
       email: "",
       password: "",
-      name: "",
-      confirmPassword: ""
     }
   });
 
-  const registerUser = handleSubmit(async (data) => {
+  const loginUser = handleSubmit(async (data) => {
     setLoading(true);
-    if (data.password !== data.confirmPassword) {
-      toast.info("Password and confirm password does not match");
-      setLoading(false);
-      return
-    }
     try {
-      const res = await apiClient.post("/users/signup", {
+      const res = await apiClient.post("/users/signin", {
         email: data.email,
-        name: data.name,
         password: data.password,
       });
+      
       if (res.data.success) {
         setLoading(false);
         toast.success(res.data.message);
-        router.push("/login");
+        // router.push() // TODO: redirect to main home page when user details will be stored
         reset();
       }
     } catch (err: any) {
@@ -55,8 +48,8 @@ export default function Signup() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 flex items-center justify-center p-10">
       <div className="w-full sm:w-xl lg:w-md shadow-lg p-6 rounded-xl">
-      <h1 className="text-xl sm:text-2xl md:text-3xl font-bold text-gray-900 mb-10 mt-4 text-center">Create Account</h1>
-        <form onSubmit={registerUser}>
+      <h1 className="text-xl sm:text-2xl md:text-3xl font-bold text-gray-900 mb-10 mt-4 text-center">Login into your account</h1>
+        <form onSubmit={loginUser}>
           <div className="flex flex-col gap-y-4">
             {/* Email input field */}
             <Input
@@ -76,21 +69,6 @@ export default function Signup() {
             />
             {errors.email && <ErrorMessage message={errors.email.message ?? ""} />}
 
-            {/* Name input field */}
-            <Input
-              label="Name"
-              labelClassName="text-lg"
-              containerClassName="flex justify-between"
-              inputClassName="w-2/3"
-              placeholder="John Doe"
-              {...register("name", {
-                required: "Name is required",
-                minLength: { value: 3, message: "Name must be at least 3 characters" },
-                maxLength: { value: 50, message: "Name must be at most 20 characters" }
-              })}
-            />
-            {errors.name && <ErrorMessage message={errors.name.message ?? ""} />}
-
             {/* Password input field */}
             <Input
               label="Password"
@@ -106,29 +84,14 @@ export default function Signup() {
               })}
             />
             {errors.password && <ErrorMessage message={errors.password.message ?? ""} />}
-
-            {/* Confirm Password input field */}
-            <Input
-              label="Confirm Pass"
-              type="password"
-              labelClassName="text-lg"
-              containerClassName="flex justify-between"
-              inputClassName="w-2/3"
-              placeholder="*******"
-              {...register("confirmPassword", {
-              required: "Confirm Password is required",
-              minLength: { value: 6, message: "Password must be at least 6 characters" },
-              maxLength: { value: 50, message: "Password must be at most 20 characters" }
-              })}
-            />
-            {errors.confirmPassword && <ErrorMessage message={errors.confirmPassword.message ?? ""} />}
+            
             <Button className={`${loading ? "pointer-events-none bg-gray-400" : ""} grid place-content-center mt-4`} type="submit" variant="dark">
               {loading ? <span className="animate-spin h-6 w-6 rounded-full border-2 border-white border-t-transparent"/> : "Sign Up"}
             </Button>
 
             <div className="mt-3">
-              <p className="text-center">Already have an account? 
-                <Button size="sm" variant="link"><Link href="/login">Login</Link></Button>
+              <p className="text-center">Don't have an account? 
+                <Button size="sm" variant="link"><Link href="/signup">Sign Up</Link></Button>
               </p>
             </div>
           </div>
