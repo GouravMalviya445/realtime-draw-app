646c90225cc5d17d686b5c5645688656ae7b9721
GouravMalviya445 <gouravmalviya445@gmail.com>
2025-03-16 17:45:01 +0530

    update rooms/create route

--- apps/web/src/app/rooms/create/page.tsx
+++ apps/web/src/app/rooms/create/page.tsx
@@ -1,11 +1,10 @@
 "use client";
-import { Form, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import { config } from "@/config";
 import { apiClient } from "@/lib/apiClient";
-import { Input } from "@repo/ui/input";
-import { Button } from "@repo/ui/button";
+import { Input } from "@/components/Input";
+import { Button } from "@/components/Button";
 import { toast } from "react-toastify";
 
 export default function CreateRoom() {
@@ -28,9 +27,9 @@ export default function CreateRoom() {
         
         if (res.data.statusCode === 201) {
           setLoading(false);
-          router.push(`/rooms/${slug}`)
           toast.success(res.data.message);
           reset({roomName: ""})
+          router.push(`/rooms/${slug}`)
         }
       } catch (err: any) {
         setTimeout(() => { 
@@ -51,7 +50,12 @@ export default function CreateRoom() {
         <div className="w-full sm:w-xl lg:w-md shadow-lg p-6 rounded-xl">
           <h1 className="text-3xl font-bold text-center mb-10">Create Room</h1>
           <form onSubmit={createRoom} className="flex flex-col gap-5">
-              <Input labelClassName="text-base sm:text-lg lg:tex-xl" containerClassName="flex justify-between" inputClassName="w-2/3" label="Room Name" type="text" {...register("roomName", {
+            <Input
+              labelClassName="text-base sm:text-lg lg:tex-xl"
+              containerClassName="flex justify-between"
+              inputClassName="w-2/3"
+              label="Room Name" type="text"
+              {...register("roomName", {
                 required: "Room is required please enter any name",
                 minLength: {
                   value: 3,
@@ -61,7 +65,9 @@ export default function CreateRoom() {
                   value: 50,
                   message: "roomName must be at least 50 char"
                 }
-              })} />
+              })}
+            />
+            
             {errors.roomName && <p className="text-red-500 text-center text-sm">{ errors.roomName.message }</p>}
             <Button type="submit" className={loading ? "pointer-events-none opacity-70 cursor-not-allowed" : ""}>
               {loading ? "Creating..." : "Create Room"}
