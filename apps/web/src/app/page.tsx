0f19a2770d40b1856bcb98c66f80516c4f7521ec
GouravMalviya445 <gouravmalviya445@gmail.com>
2025-03-18 00:45:14 +0530

    add

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,8 +1,36 @@
 // "use client";
 import { LandingPage } from "@/components/LandingPage";
-import { Button } from "@repo/ui/button"
+import { apiClient } from "@/lib/apiClient";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+async function isAuthenticated(): Promise<boolean> {
+  const token = (await cookies()).get("accessToken")?.value;
+  if (!token) {
+    return false;
+  }
+  try {
+    const res = await apiClient.get("/users/current-user", {
+      headers: {
+        "Cookie": `accessToken=${token}`
+      }
+    });
+    if (res.data.success) {
+      return true;
+    } else {
+      return false
+    }
+  } catch (error) {
+    console.log("Error while getting current user", error);
+    return false;
+  }
+}
+
+export default async function Home() {
+  const isAuth = await isAuthenticated();
+  if (isAuth) {
+    return redirect("/rooms/create");
+  }
   return (
     <LandingPage /> 
   )
