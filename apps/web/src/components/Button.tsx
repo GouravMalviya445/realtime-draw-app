7a4779b07159adeee40bd65e8c5fcb9f9a576ef6
GouravMalviya445 <gouravmalviya445@gmail.com>
2025-03-18 00:45:42 +0530

    update button comp

--- apps/web/src/components/Button.tsx
+++ apps/web/src/components/Button.tsx
@@ -6,8 +6,8 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>{
   children: ReactNode;
   type?: "button" | "submit" | "reset";
   className?: string;
-  variant?: "primary" | "secondary" | "danger" | "success" | "warning" | "light" | "dark" | "link";
-  size?: "sm" | "md" | "lg";
+  variant?: "primary" | "secondary" | "danger" | "success" | "warning" | "light" | "dark" | "link" | "simple";
+  size?: "sm" | "md" | "lg" | "default";
   startIcon?: ReactNode;
   endIcon?: ReactNode;
 }
@@ -21,6 +21,7 @@ const variants = {
   "light": "bg-gray-200 hover:bg-gray-300 text-gray-900 rounded-md focus:ring-gray-300",
   "dark": "bg-gray-800 hover:bg-gray-900 text-white rounded-md focus:ring-gray-700",
   "link": "text-blue-500 hover:text-blue-600 hover:underline focus:outline-none",
+  "simple": "bg-transparent hover:underline focus:outline-none"
 };
 
 const defaultClasses = "font-semibold cursor-pointer focus:outline-none focus:ring focus:ring-opacity-50 transition-all ease-in"
@@ -29,6 +30,7 @@ const sizes = {
   "sm": "px-4 py-1",
   "md": "px-6 py-2 text-md",
   "lg": "px-8 py-2 text-lg",
+  "default": "px-1 py-1"
 }
 
 export const Button = ({
@@ -36,7 +38,7 @@ export const Button = ({
   type = "button",
   className = "",
   variant = "primary",
-  size = "md",
+  size = "default",
   startIcon,
   endIcon
 }: ButtonProps) => {
