565bbcc8d96255de3db5e0c1b7f482f65f61dfbd
GouravMalviya445 <gouravmalviya445@gmail.com>
2025-03-18 00:48:57 +0530

    add useSocket hook and initDraw for canvas

--- apps/web/src/draw/index.ts
+++ apps/web/src/draw/index.ts
@@ -1,7 +1,44 @@
-export function initDraw(ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement) {
-  ctx.fillStyle = "#2424424";
+import { apiClient } from "@/lib/apiClient";
+
+type Shape = {
+  type: "rectangle";
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+} | {
+  type: "circle";
+  centerX: number;
+  centerY: number;
+  radius: number;
+}
+
+export async function initDraw(canvas: HTMLCanvasElement, roomId: number, socket: WebSocket) {
+  const ctx = canvas.getContext("2d");
+  if (!ctx) return;
+
+  ctx.fillStyle = "#242424";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   
+  const existingShapes: (Shape[] | null) = await getExistingShapes(roomId);
+  clearCanvas(existingShapes as Shape[], ctx, canvas);
+
+
+  socket.onmessage = (event) => {
+    try {
+      const message = JSON.parse(event.data);
+      if (message.type === "chat") {
+        const parsedMessage = JSON.parse(message.message);
+        existingShapes?.push(parsedMessage);
+        if (existingShapes) {
+          clearCanvas(existingShapes, ctx, canvas);
+        }
+      }
+    } catch (err) {
+      
+    }
+  }
+    
   let clicked = false;
   let startX = 0;
   let startY = 0;
@@ -18,6 +55,29 @@ export function initDraw(ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElemen
   function handleMouseUp(e: MouseEvent) {
     clicked = false;
     console.log(e.clientX, e.clientY);
+    const width = e.clientX - startX;
+    const height = e.clientY - startY;
+    
+    const shape: Shape = {
+      type: "rectangle",
+      x: startX,
+      y: startY,
+      width,
+      height
+    }
+    existingShapes && existingShapes.push(shape);
+
+    try {
+      const message = JSON.stringify({
+        type: "chat",
+        roomId,
+        message: JSON.stringify(shape)
+      })
+      socket.send(message);  
+    } catch (err) {
+      console.log("Error while sending shape", err);      
+    }
+
   }
 
   // handle mouse move
@@ -28,9 +88,10 @@ export function initDraw(ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElemen
       const width = e.clientX - startX;
       if (!ctx) return;
       
-      ctx?.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.fillStyle = "#2424424";
-      ctx.fillRect(0, 0, canvas.width, canvas.height);
+ 
+      if (existingShapes) {
+        clearCanvas(existingShapes, ctx, canvas)
+      }
       ctx.strokeStyle = "white"
       ctx?.strokeRect(startX, startY, width, height);
     }
@@ -44,5 +105,33 @@ export function initDraw(ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElemen
     canvas.removeEventListener("mousedown", handleMouseDown);
     canvas.removeEventListener("mouseup", handleMouseUp);
     canvas.removeEventListener("mousemove", handleMouseMove);
+  }
+}
+
+function clearCanvas(existingShapes: Shape[], ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);  
+  ctx.fillStyle = "#242424";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  existingShapes.map((shape) => {
+    if (shape.type === "rectangle") {
+      ctx.strokeStyle = "white";
+      ctx.strokeRect(shape.x, shape.y, shape.width, shape.height);
+    }
+  })
+}
+
+async function getExistingShapes(roomId: number): (Promise<Shape[] | null>) {
+  try {
+    const res = await apiClient(`/rooms/chat/${roomId}`);
+    if (res.data.success) {
+      const chats = res.data.data.chats.map((chat: any) => JSON.parse(chat.message));
+      return chats;
+    } else {
+      return null;
+    }
+  } catch (err) {
+    console.log("Error in getting existing shapes", err);
+    return null;
   }
 }
---
+++ apps/web/src/hooks/useSocket.ts
@@ -0,0 +1,43 @@
+import { config } from "@/config";
+import { useEffect, useRef, useState } from "react";
+
+export function useSocket(roomId: number, token?: string) {
+  const [loading, setLoading] = useState(true);
+  const socket = useRef<WebSocket | null>(null);
+  
+  useEffect(() => {
+    if (typeof config.wsServerUrl !== "string") return;
+
+    console.log(token);
+
+    // pass the accessToken to query params in the url
+    socket.current = new WebSocket(`${config.wsServerUrl}?accessToken=${token}&transport=websocket`);
+
+    socket.current.onopen = () => {
+      setLoading(false);
+      console.log("WebSocket connection opened");
+      socket.current?.send(JSON.stringify({
+        type: "join-room",
+        roomId
+      }))
+    }
+
+    socket.current.onclose = (ev) => {
+      console.log("WebSocket connection closed", ev);
+    }
+
+    socket.current.onerror = (err) => {
+      console.log("WebSocket connection error", err);
+    }
+
+    return () => {
+      socket.current?.close();
+    }
+    
+  }, [])
+
+  return {
+    loading,
+    socket,
+  }
+}
